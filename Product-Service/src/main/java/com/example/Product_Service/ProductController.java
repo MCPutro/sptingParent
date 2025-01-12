3165bb7446951db17345765553f73c3cd93fff8a
Mu'ti C Putro <mcpoetro@gmail.com>
2025-01-12 19:11:04 +0700

    asd

--- Product-Service/src/main/java/com/example/Product_Service/ProductController.java
+++ Product-Service/src/main/java/com/example/Product_Service/ProductController.java
@@ -15,7 +15,7 @@ public class ProductController {
     public List<Product> getProducts() {
         if (products == null) {
             products = new ArrayList<>();
-            this.products.add(new Product(1,"piring pecah lagi 1",123));
+            this.products.add(new Product(1,"piring pecah lagi 12",123));
             this.products.add(new Product(2,"gelas",123));
             this.products.add(new Product(3,"sendok",123));
             this.products.add(new Product(4,"garpu",123));

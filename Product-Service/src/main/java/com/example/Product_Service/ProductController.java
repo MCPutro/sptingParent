c2b53814a58886c587a9b13aeb85966ada505562
Mu'ti  C Putro <mcpoetro@gmail.com>
2025-01-12 16:22:38 +0000

    Edit ProductController.javaasd

--- Product-Service/src/main/java/com/example/Product_Service/ProductController.java
+++ Product-Service/src/main/java/com/example/Product_Service/ProductController.java
@@ -24,7 +24,7 @@ public class ProductController {
         if (this.products.isEmpty()) {
             this.products.add(new Product(1,"piring",123));
             this.products.add(new Product(2,"gelas",123));
-            this.products.add(new Product(3,"sendok",123));
+            this.products.add(new Product(3,"sendok asda",123));
             this.products.add(new Product(4,"garpu",123));
         }
 
